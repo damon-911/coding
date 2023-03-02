140c698e32ff1b56d26d55060a642913b201c25e
Minjun Kim <damon911@naver.com>
2023-03-02 17:28:21 +0900

    백준 9202번 풀이

--- boj/data_structure/p9202/Main.java
+++ boj/data_structure/p9202/Main.java
@@ -1,12 +1,11 @@
 package boj.data_structure.p9202;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] mx = {-1, 1, 0, 0, -1, -1, -1, 1};
-    static int[] my = {0, 0, -1, 1, -1, -1, 1, 1};
+    static int[] MX = {-1, 1, 0, 0, -1, 1, -1, 1};
+    static int[] MY = {0, 0, -1, 1, -1, -1, 1, 1};
 
     static int W, B;
     static String[] words;
@@ -22,19 +21,28 @@ public class Main {
     static TrieNode root = new TrieNode();
 
     static class TrieNode {
-        TrieNode[] child = new TrieNode[26];
+        TrieNode[] child;
         boolean isEnd;
         boolean isHit;
 
-        boolean hasChild(char c) {
+        public TrieNode() {
+            this.child = new TrieNode[26];
+            this.isEnd = false;
+            this.isHit = false;
+        }
+
+        // 해당 c가 자식으로 있는지 확인
+        public boolean hasChild(char c) {
             return child[c - 'A'] != null;
         }
 
-        TrieNode getChild(char c) {
+        // 해당 c를 나타내는 자식 리턴
+        public TrieNode getChild(char c) {
             return child[c - 'A'];
         }
 
-        void clearHit() {
+        // root 초기화할 때 사용
+        public void clearHit() {
             this.isHit = false;
             for (TrieNode trieNode : child) {
                 if (trieNode != null)
@@ -43,10 +51,18 @@ public class Main {
         }
     }
 
-    static void search(int y, int x, TrieNode node) {
+    static int compare(String s1, String s2) {
+        int result = s2.length() - s1.length();
+        if (result == 0)
+            return s1.compareTo(s2);
+
+        return result;
+    }
+
+    static void search(int x, int y, TrieNode node) {
         // 1. 체크인
-        visited[y][x] = true;
-        sb.append(board[y][x]);
+        visited[x][y] = true;
+        sb.append(board[x][y]);
 
         // 2. 목적지에 도달하였는가?
         if (node.isEnd && !node.isHit) {
@@ -61,41 +77,37 @@ public class Main {
 
         // 3. 연결된 곳을 순회
         for (int i = 0; i < 8; i++) {
-            int ty = y + my[i];
-            int tx = x + mx[i];
+            int tx = x + MX[i];
+            int ty = y + MY[i];
 
-            // 4. 가능한가? (맵을 벗어나지 않고, Trie에 단어가 있고, 방문한적이 없고)
-            if ( 0 <= ty && ty < 4 && 0 <= tx && tx < 4) {
-                if (node.hasChild(board[ty][tx]) && !visited[ty][tx]) {
+            // 4. 갈 수 있는가? (맵을 벗어나지 않고, 트라이에 단어가 있고, 방문한 적이 없고)
+            if ( 0 <= tx && tx < 4 && 0 <= ty && ty < 4) {
+                if (node.hasChild(board[tx][ty]) && !visited[tx][ty]) {
                     // 5. 간다
-                    search(ty, tx, node.getChild(board[ty][tx]));
+                    search(tx, ty, node.getChild(board[tx][ty]));
                 }
             }
         }
 
         // 6. 체크아웃
-        visited[y][x] = false;
+        visited[x][y] = false;
         sb.deleteCharAt(sb.length() - 1);
-    }
-
-    static int compare(String s1, String s2) {
-        int result = s2.length() - s1.length();
-        if (result == 0)
-            return s1.compareTo(s2);
-
-        return result;
     }
 
     static void insert(String word) {
         TrieNode current = root;
+
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
+            // 해당 알파벳을 사전에 처음 등재할 경우
             if (!current.hasChild(c)) {
                 current.child[c - 'A'] = new TrieNode();
             }
+            // 단어를 잇는 과정
             current = current.getChild(c);
         }
 
+        // 단어의 끝 표시함
         current.isEnd = true;
     }
 
@@ -103,16 +115,16 @@ public class Main {
         System.setIn(new FileInputStream("boj/data_structure/p9202/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        W = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(br.readLine());
 
+        // 단어 저장
         words = new String[W];
         for (int i = 0; i < W; i++) {
-            st = new StringTokenizer(br.readLine());
-            words[i] = st.nextToken();
+            words[i] = br.readLine();
             insert(words[i]);
         }
 
+        // 점수 설정
         scores = new int[9];
         scores[0] = 0; scores[1] = 0; scores[2] = 0;
         scores[3] = 1; scores[4] = 1;
@@ -120,8 +132,7 @@ public class Main {
 
         br.readLine();
 
-        st = new StringTokenizer(br.readLine());
-        B = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(br.readLine());
         for (int num = 0; num < B; num++) {
             sum = 0;
             answer = "";
@@ -129,8 +140,7 @@ public class Main {
 
             board = new char[4][4];
             for (int i = 0; i < 4; i++) {
-                st = new StringTokenizer(br.readLine());
-                String temp = st.nextToken();
+                String temp = br.readLine();
                 for (int j = 0; j < 4; j++) {
                     board[i][j] = temp.charAt(j);
                 }
@@ -140,7 +150,9 @@ public class Main {
 
             for (int i = 0; i < 4; i++) {
                 for (int j = 0; j < 4; j++) {
-                    search(i, j, root);
+                    if (root.hasChild(board[i][j])) {
+                        search(i, j, root.getChild(board[i][j]));
+                    }
                 }
             }
 
