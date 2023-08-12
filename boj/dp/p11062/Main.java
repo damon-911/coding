e703408f3ca87a6202025fcda6b011e7201a28fe
Minjun Kim <damon911@naver.com>
2023-08-12 15:57:51 +0900

    백준 11062번 풀이

--- boj/dp/p11062/Main.java
+++ boj/dp/p11062/Main.java
@@ -6,8 +6,27 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int T, N;
-    static int[] nums;
-    static int[][] dp;
+    static int[] cards;
+    static int[][][] dp;
+
+    public static int game(int turn, int start, int end) {
+        if (start > end) {
+            return 0;
+        }
+
+        if (dp[turn][start][end] != 0) {
+            return dp[turn][start][end];
+        }
+
+        if (turn == 0) { // 근우 차례인 경우는 왼쪽 카드를 뽑았을 떄와 오른쪽 카드를 뽑았을 때 중 숫자의 합이 크도록 설정
+            dp[turn][start][end] = Math.max(game(turn + 1, start + 1, end) + cards[start],
+                    game(turn + 1, start, end - 1) + cards[end]);
+        } else { // 명우 차례인 경우는 왼쪽 카드를 뽑았을 때와 오른쪽 카드를 뽑았을 때 중 숫자의 합이 작도록 설정
+            dp[turn][start][end] = Math.min(game(turn - 1, start + 1, end), game(turn - 1, start, end - 1));
+        }
+
+        return dp[turn][start][end];
+    }
 
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("boj/dp/p11062/input.txt"));
@@ -18,13 +37,16 @@ public class Main {
         for (int tc = 0; tc < T; tc++) {
             N = Integer.parseInt(br.readLine());
 
-            nums = new int[N + 1];
+            cards = new int[N + 1];
 
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= N; i++) {
-                nums[i] = Integer.parseInt(st.nextToken());
+                cards[i] = Integer.parseInt(st.nextToken());
             }
 
+            dp = new int[2][N + 1][N + 1]; // 근우와 명우의 차례, 카드의 맨 왼쪽, 카드의 맨 오른쪽
+
+            System.out.println(game(0, 1, N));
         }
     }
 }
