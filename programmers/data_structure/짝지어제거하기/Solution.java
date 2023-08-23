1f53ac840dd31828fbd145eec24cea9d1cc7c4ae
Minjun Kim <damon911@naver.com>
2023-08-23 13:09:24 +0900

    프로그래머스 짝지어 제거하기 풀이

---
+++ programmers/data_structure/짝지어제거하기/Solution.java
@@ -0,0 +1,32 @@
+package programmers.data_structure.짝지어제거하기;
+
+import java.util.Stack;
+
+public class Solution {
+
+    public static int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (stack.isEmpty()) {
+                stack.push(c);
+            } else {
+                if (stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        String s = "baabaa";
+
+        System.out.println(solution(s));
+    }
+}
