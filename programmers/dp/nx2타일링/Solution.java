e8cafea4fc3a9d08f288234b8003f7a83b7f1459
Minjun Kim <damon911@naver.com>
2023-09-05 13:53:29 +0900

    프로그래머스 2 x n 타일링 풀이

---
+++ programmers/dp/nx2타일링/Solution.java
@@ -0,0 +1,23 @@
+package programmers.dp.nx2타일링;
+
+public class Solution {
+
+    public static int solution(int n) {
+        int[] dp = new int[n + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+
+        System.out.println(solution(n));
+    }
+}
