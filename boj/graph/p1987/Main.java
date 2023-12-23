0456b72bad4929e854a7eb3d77549c8bad2d3540
Minjun Kim <damon911@naver.com>
2023-12-23 13:08:27 +0900

    백준 1987번 풀이

--- boj/graph/p1987/Main.java
+++ boj/graph/p1987/Main.java
@@ -1,6 +1,8 @@
-package boj.dfs.p1987;
+package boj.graph.p1987;
 
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,45 +12,44 @@ public class Main {
 
     static int R, C;
     static int result = 0;
-    static String[][] board;
+    static char[][] board;
+    static Set<Character> visitedSet = new HashSet<>();
 
-    static void dfs(int x, int y, int depth, String path) {
-        if (path.contains(board[x][y])) {
-            result = Math.max(result, depth);
-            return;
-        }
-
-        path += board[x][y];
+    static void dfs(int x, int y, int depth) {
+        result = Math.max(result, depth);
 
         for (int i = 0; i < 4; i++) {
             int tx = x + MX[i];
             int ty = y + MY[i];
 
-            if (tx < 0 || ty < 0 || tx >= R || ty >= C)
+            if (tx < 0 || ty < 0 || tx >= R || ty >= C || visitedSet.contains(board[tx][ty]))
                 continue;
 
-            dfs(tx, ty, depth + 1, path);
+            visitedSet.add(board[tx][ty]);
+            dfs(tx, ty, depth + 1);
+            visitedSet.remove(board[tx][ty]);
         }
     }
 
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("boj/dfs/p1987/input.txt"));
+        System.setIn(new FileInputStream("boj/graph/p1987/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        board = new String[R][C];
+        board = new char[R][C];
 
         for (int i = 0; i < R; i++) {
             String str = br.readLine();
             for (int j = 0; j < C; j++) {
-                board[i][j] = String.valueOf(str.charAt(j));
+                board[i][j] = str.charAt(j);
             }
         }
 
-        dfs(0, 0, 0, "");
+        visitedSet.add(board[0][0]);
+        dfs(0, 0, 1);
 
         System.out.println(result);
     }
